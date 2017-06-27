d44d9bb22576e536bf941375028cedc3d61b6336
dhn <dhn610018574@sina.com>
2017-06-27 11:02:47 +0800

    02-展示列表和创建任务完成

--- js/app.js
+++ js/app.js
@@ -1,6 +1,32 @@
-(function (window) {
+!function (angular) {
 	'use strict';
+	angular
+		.module('todoApp',[])
+		.controller('TodoController',['$scope',TodoController]);
 
-	// Your starting point. Enjoy the ride!
+		function TodoController($scope){
+			var vm = $scope;
+			// 1、展示任务列表
+			var todoList = [
+				{id:1,name:'吃饭',isCompleted:false},
+				{id:2,name:'睡觉',isCompleted:false},
+				{id:3,name:'打豆豆',isCompleted:false}
+			];
+			vm.todoList = todoList;
+			//2.添加任务
+			vm.taskName = '';
+			vm.add = function(){
+				if(vm.taskName.trim() === '') return;
+				var id;
+				if(todoList.length === 0 ){
+					id = 1;
+				}else{
+					id = todoList[ todoList.length - 1 ].id + 1;
+				}
+				todoList.push({id:id,name:vm.taskName,isCompleted:false});
+				vm.taskName = '';
+			}
 
-})(window);
+		}
+	
+}(angular);
